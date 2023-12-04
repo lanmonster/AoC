e320b22fad1aa1198c5294dcdd743c63704a96b1
Kyle Lanmon <kyle.lanmon@gmail.com>
2023-12-04 10:36:12 -0600

    2023: Initial commit

---
+++ 2023/meta/new_day.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+NEW_DAY_NUM=$(find . -type d -name "day*" | wc -l | tr -d ' ' | awk '{print $1+1}')
+
+mkdir "day$NEW_DAY_NUM"
+ln -s utils.jakt "day$NEW_DAY_NUM/utils.jakt"
+touch "day$NEW_DAY_NUM/input.txt" "day$NEW_DAY_NUM/test.txt" "day$NEW_DAY_NUM/test2.txt"
+
+cat <<EOF >"day$NEW_DAY_NUM/day$NEW_DAY_NUM.jakt"
+comptime part1_expected_value() => 0u64
+comptime part2_expected_value() => 0u64
+
+fn part1(lines: [String]) throws -> u64 {
+    abort()
+}
+fn part2(lines: [String]) throws -> u64 {
+    abort()
+}
+EOF
+
+sed -i'.jakt' "s|^import utils.*|import day$NEW_DAY_NUM::day$NEW_DAY_NUM as d$NEW_DAY_NUM\n&|" aoc.jakt
+sed -i'.jakt' "s|\( *\)else => {|\1\"$NEW_DAY_NUM\" => (d$NEW_DAY_NUM::part1_expected_value(), d$NEW_DAY_NUM::part1(lines), d$NEW_DAY_NUM::part2_expected_value(), d$NEW_DAY_NUM::part2(lines))\n&|" aoc.jakt
+rm aoc.jakt.jakt
